31690dab1de977d1bcc511d30d3b0658afdd0591
serghavr <serghavr@gmail.com>
2026-01-08 10:25:07 +0200

    Add JS HW1

---
+++ 2_JavaScript/HW/HW_1/HW_1-2/js/script.js
@@ -0,0 +1,5 @@
+let firstName = `Сергій`;
+let middleName = `Олександрович`;
+let lastName = `Гаврищук`
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
---
+++ 2_JavaScript/HW/HW_1/HW_1-4/js/script.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Введіть ваше ім'я:");
+let middleName = prompt("Введіть ваше по батькові:");
+let age = +prompt("Скільки вам років?");
+console.log(`Користувач: ${firstName} ${middleName}`);
+console.log(`Вік: ${age} років`);
